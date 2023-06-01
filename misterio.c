495842a5ae1016fe66d2e0e691527a281aec7ff2
makmadon <stlaintotofranco@gmail.com>
2023-06-01 15:47:51 -0400

    .

--- misterio.c
+++ misterio.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 
-int misterio(int x,int y){
+int misterio(int x, int y)
+{
     int t;
-    if(!y)
+    if (!y)
         return 1;
-    t=misterio(x,y>>1);
-    if(y&1)
-        return t*t*x;
-    return t*t;
+    t = misterio(x, y >> 1);
+    if (y & 1)
+        return t * t * x;
+    return t * t;
 }
 
-int y=2;
+int y = 2;
 
-
-int main(){
-    printf("%d", misterio(y,4));
+int main()
+{
+    printf("%d", misterio(y, 4));
 }
