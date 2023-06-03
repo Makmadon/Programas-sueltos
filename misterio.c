7b7989538ffb78982fca8925d6d83859b473a5ba
makmadon <stlaintotofranco@gmail.com>
2023-06-03 10:02:07 -0400

    remove everything

--- misterio.c
+++ misterio.c
@@ -1,15 +1,7 @@
 #include <stdio.h>
 
-int misterio(unsigned n)
-{
-    if (!n)
-        return n;
-    return misterio(n >> 1) * 10 + (n & 1);
-}
-
-char *hola = "Hola";
-
 int main()
 {
-    printf("%s", hola);
+
+    return 0;
 }
